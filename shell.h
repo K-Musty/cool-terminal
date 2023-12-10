62c2ea1a236a3284ef7a024b93932e3ca389baea
K-Musty <kmustapha9564@gmail.com>
2023-12-09 16:04:10 -0800

    This commit would add prompt,parser and co

--- shell.h
+++ shell.h
@@ -8,7 +8,17 @@
 #include <limits.h>
 #include <stdbool.h>
 
-
+#define MAX_ARGUMENTS 50
+/**
+ * cool_command - command struct reciever
+ *@command_name: command name pointer
+ *@cool_arg: argument poointer
+ */
+typedef struct {
+    char *command_name;
+    char *cool_arg[MAX_ARGUMENTS];
+    // Add more fields for redirection info, flags, etc. if needed
+} cool_command;
 
 
 
